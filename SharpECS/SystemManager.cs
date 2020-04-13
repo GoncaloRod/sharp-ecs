d46c772497e226a91a0478f84dfd78f5762a8640
Gonçalo Rodrigues <rodriguesgonalo604@gmail.com>
2020-04-13 10:57:56 +0100

    Created a system manager

---
+++ SharpECS/SystemManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SharpECS
+{
+    public class SystemManager
+    {
+        private List<System> _systems;
+
+        public SystemManager(IEnumerable<System> systems)
+        {
+            _systems.AddRange(systems);
+        }
+    }
+}
